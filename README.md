073d1585ed70a593674f7934a61179346a0a3603
drusk <drusk@uvic.ca>
2014-03-13 11:07:31 -0700

    Added ability to build full CSV for test data. Updated pipeline in README.

--- README.md
+++ README.md
@@ -9,10 +9,9 @@ Pipeline
 ```
 ./build_db.py data/  # output: sales.db
 ./build_full_csv.py sales.db  # output: sales.csv
+./build_full_csv.py sales.db --test # output: test.csv
 ./extract_features.py sales.csv train.num.csv  # output: train.num.csv
+./extract_features.py test.csv test.num.csv  # output: test.num.csv
 ./train_svr.py train.num.csv svr.model  # output: svr.model
-./extract_features.py test.csv test.num.csv  # output: test.num.csv
-./build_full_csv.py --test sales.db  # output: test.csv
-./extract_features.py test.csv test.num.csv  # output: test.num.csv
 ./predict.py svr.model test.num.csv predictions  # output: predictions
 ```
--- build_full_csv.py
+++ build_full_csv.py
@@ -11,29 +11,46 @@ import sqlite3
 
 
 class CsvBuilder(object):
-    def __init__(self, dbname, output_filename):
+    def __init__(self, dbname, output_filename, test=False):
         if output_filename is None:
-            self.filename = os.path.splitext(dbname)[0] + ".csv"
+            if not test:
+                self.filename = os.path.splitext(dbname)[0] + ".csv"
+            else:
+                self.filename = "test.csv"
         else:
             self.filename = output_filename
 
         self.con = sqlite3.connect(dbname)
+        self.test = test
 
     def join_tables(self):
         cur = self.con.cursor()
 
-        cur.execute(
-            """
-            SELECT S.store_id, S.type, S.size, F.date, F.temperature,
-                   F.fuel_price, F.markdown1, F.markdown2, F.markdown3,
-                   F.markdown4, F.markdown5, F.cpi, F.unemployment,
-                   F.is_holiday, ST.weekly_sales
-            FROM Stores S, Features F, SalesTrain ST
-            WHERE S.store_id = F.store_id AND
-                  F.store_id = ST.store_id AND
-                  F.date = ST.date
-            """
-        )
+        if self.test:
+            sql = """
+                  SELECT S.store_id, S.type, S.size, F.date, F.temperature,
+                         F.fuel_price, F.markdown1, F.markdown2, F.markdown3,
+                         F.markdown4, F.markdown5, F.cpi, F.unemployment,
+                         F.is_holiday
+                  FROM Stores S, Features F, SalesTest ST
+                  WHERE S.store_id = F.store_id AND
+                        F.store_id = ST.store_id AND
+                        F.date = ST.date
+                  """
+
+        else:
+            sql = """
+                  SELECT S.store_id, S.type, S.size, F.date, F.temperature,
+                         F.fuel_price, F.markdown1, F.markdown2, F.markdown3,
+                         F.markdown4, F.markdown5, F.cpi, F.unemployment,
+                         F.is_holiday, ST.weekly_sales
+                  FROM Stores S, Features F, SalesTrain ST
+                  WHERE S.store_id = F.store_id AND
+                        F.store_id = ST.store_id AND
+                        F.date = ST.date
+                  """
+
+        cur.execute(sql)
 
         return cur
 
@@ -54,10 +71,11 @@ def main():
                         help="The name of the output file.  "
                              "Defaults to the input filename with its "
                              "extension replaced by .csv")
+    parser.add_argument("--test", action="store_true")
 
     args = parser.parse_args()
 
-    builder = CsvBuilder(args.dbname, args.output_filename)
+    builder = CsvBuilder(args.dbname, args.output_filename, args.test)
     builder.build()
 
 
