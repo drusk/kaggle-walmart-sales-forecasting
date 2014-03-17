0577a547c830591217afb28e12f8add96af9f729
drusk <drusk@uvic.ca>
2014-03-17 16:18:53 -0700

    Added parameter for proportion of dataset to use as test set in evaluation.

--- evaluate.py
+++ evaluate.py
@@ -12,12 +12,12 @@ from sklearn import cross_validation
 
 
 class ModelEvaluator(object):
-    def __init__(self, data):
+    def __init__(self, data, test_size):
         (self.train_data,
          self.test_data,
          self.train_target,
          self.test_target) = cross_validation.train_test_split(
-            data[:, :-1], data[:, -1])
+            data[:, :-1], data[:, -1], test_size=test_size)
 
     def mean_absolute_error(self, model):
         model.fit(self.train_data, self.train_target)
@@ -31,6 +31,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("model", help="The model to evaluate.")
     parser.add_argument("data", help="Labelled data used for evaluation.")
+    parser.add_argument("--test-size", dest="test_size", type=float,
+                        default=0.7,
+                        help="The proportion of the dataset to include in "
+                             "the test split.")
 
     args = parser.parse_args()
 
@@ -40,7 +44,7 @@ def main():
     with open(args.data) as filehandle:
         data = np.loadtxt(filehandle, delimiter=",")
 
-    evaluator = ModelEvaluator(data)
+    evaluator = ModelEvaluator(data, args.test_size)
 
     print "Mean squared error: %.5f" % evaluator.mean_absolute_error(model)
 
